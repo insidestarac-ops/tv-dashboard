84e3a0ede4bc23a74f58794aa1b4f16deff1a92b
insidestarac-ops <insidestarac@gmail.com>
2026-02-16 15:55:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SCENES = [
   { file: "scenes/news.html", duration: 45000 },
-  { file: "scenes/meteo.html", duration: 30000 },
+  { file: "scenes/meteo.html", duration: 35000 },
   { file: "scenes/calendar.html", duration: 20000 }
 ];
