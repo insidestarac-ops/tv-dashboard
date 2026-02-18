5bb767bd5617f1f4a706ab3f6e59d3edd53b86c1
insidestarac-ops <insidestarac@gmail.com>
2026-02-18 17:20:48 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const SCENES = [
+  { file: "scenes/news.html", duration: 6 * 12000 }, // Environ 1m12 pour lire les 6 actus
+  { file: "scenes/meteo.html", duration: 30000 },    // 30 secondes pour la météo
+  { file: "scenes/calendar.html", duration: 30000 }  // 30 secondes pour le calendrier
+];
